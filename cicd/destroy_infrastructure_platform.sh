537632c6e9fafab583ba01931a8ce118d430e24a
Dr. Malte Harland <mharland3@gmail.com>
2024-08-24 15:34:56 +0200

    set env for destroy platform

--- cicd/destroy_infrastructure_platform.sh
+++ cicd/destroy_infrastructure_platform.sh
@@ -13,6 +13,18 @@ sleep 5
 #az resource delete -g "${TF_VAR_rg_name}" --name "$(az resource list --query "[?contains(name, 'Failure Anomalies')].name" -o tsv)" --resource-type "microsoft.alertsmanagement/smartDetectorAlertRules"
 
 cd ${PROJECT_ROOT}/infrastructure/platform
+export ARM_CLIENT_ID=$(cat ${PROJECT_ROOT}/secrets/devops_sp_client_id.txt)
+export ARM_CLIENT_SECRET=$(cat ${PROJECT_ROOT}/secrets/devops_sp_client_secret.txt)
+export ARM_TENANT_ID=$(cat ${PROJECT_ROOT}/secrets/tenant_id.txt)
+export ARM_SUBSCRIPTION_ID=${SUBSCRIPTION_ID}
+
+# az account set -s ${SUBSCRIPTION_ID}
+export TF_VAR_tenant_id=$ARM_TENANT_ID
+echo "Current tenant: ${TF_VAR_tenant_id}"
+echo "Current subscription: ${ARM_SUBSCRIPTION_ID}"
+echo "Current environment: ${ENV_NAME}"
+
+cd ${PROJECT_ROOT}/infrastructure/platform
 terraform init \
     -backend-config="resource_group_name=${TF_STATE_RESOURCE_GROUP_NAME}" \
     -backend-config="storage_account_name=${TF_STATE_STORAGE_ACCOUNT_NAME}"
