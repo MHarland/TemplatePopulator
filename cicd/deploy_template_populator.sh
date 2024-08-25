f87af1483362fee2367e9f291e9ef698a0d3f5a0
Dr. Malte Harland <mharland3@gmail.com>
2024-08-25 14:04:32 +0200

    fix deploy_template_populator

--- cicd/deploy_template_populator.sh
+++ cicd/deploy_template_populator.sh
@@ -13,7 +13,7 @@ export ARM_CLIENT_SECRET=$(cat ${PROJECT_ROOT}/secrets/devops_sp_client_secret.t
 export ARM_TENANT_ID=$(cat ${PROJECT_ROOT}/secrets/tenant_id.txt)
 export URL=${TF_VAR_acr_name}.azurecr.io/${TF_VAR_image_name}:${TF_VAR_image_tag}
 
-az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+az login --service-principal -u "${ARM_CLIENT_ID}" -p=${ARM_CLIENT_SECRET} --tenant "${ARM_TENANT_ID}"
 az acr login --name ${TF_VAR_acr_name}
 sudo docker login -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET ${TF_VAR_acr_name}.azurecr.io
 sudo docker build -t $URL -f ${PROJECT_ROOT}/az_func/Dockerfile ${PROJECT_ROOT}
